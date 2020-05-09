5b81344afb1a0d340792f93c1ed89947277ce929
dongshuhao <746564612@qq.com>
2020-05-09 09:17:24 +0800

    request 冲突

--- routes/admin_info.js
+++ routes/admin_info.js
@@ -31,26 +31,28 @@ console.log(arg1.phone); */
 }) */
 //登录
 router.post('/login', function (req, res, next) {
+    var newRequest = request;
     var arg = req.body;
     var _data = { phone: arg.phone, password: utility.md5(arg.password) };
     data.connect(function (db) {
         db.collection('admin_info').find(_data).toArray(function (err, docs) {
             if (err) {
-                request.state = -1;
-                request.message = err;
-                res.json(request);
+                newRequest.state = -1;
+                newRequest.message = err;
+                res.json(newRequest);
             } else {
                 if (docs.length > 0) {
-                    request.data = docs[0];
-                    res.json(request);
+                    newRequest.state = 1;
+                    newRequest.data = docs[0];
+                    res.json(newRequest);
 
                 } else {
 
-                    request.state = -1;
-                    request.message = {
+                    newRequest.state = -1;
+                    newRequest.message = {
                         name:"账号或密码错误"
                     };;
-                    res.json(request);
+                    res.json(newRequest);
 
                 }
 
@@ -60,22 +62,23 @@ router.post('/login', function (req, res, next) {
 }),
     //保存管理员信息
     router.post('/saveadmininfo', function (req, res, next) {
+        var newRequest = request;
         var arg = req.body;
         data.connect(function (db) {
             db.collection('admin_info').find({ phone: arg.phone }).toArray(function (err, docs) {
                 if (err) {
-                    request.state = -1;
-                    request.message = err;
-                    res.json(request);
+                    newRequest.state = -1;
+                    newRequest.message = err;
+                    res.json(newRequest);
                 } else {
                     if (docs.length > 0) {
-                        request.state = -1;
-                        request.data = docs;
+                        newRequest.state = -1;
+                        newRequest.data = docs;
 
-                        request.message = {
+                        newRequest.message = {
                             name:"重复账号"
                         };
-                        res.json(request);
+                        res.json(newRequest);
                     } else {
                         var current_time = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
                         var admininfosavemodel = {
@@ -94,9 +97,9 @@ router.post('/login', function (req, res, next) {
                         data.connect(function (db) {
                             db.collection('admin_info').find({}).sort({ _id: -1 }).limit(1).toArray(function (err, docs) {
                                 if (err) {
-                                    request.state = -1;
-                                    request.message = err;
-                                    res.json(request);
+                                    newRequest.state = -1;
+                                    newRequest.message = err;
+                                    res.json(newRequest);
                                 } else {
                                     if (docs.length > 0) {
                                         admininfosavemodel.id = docs[0].id + 1;
@@ -104,11 +107,12 @@ router.post('/login', function (req, res, next) {
                                     data.connect(function (db) {
                                         db.collection('admin_info').insertOne(admininfosavemodel, function (err, result) {
                                             if (err) {
-                                                request.state = -1;
-                                                request.message = err;
-                                                res.json(request);
+                                                newRequest.state = -1;
+                                                newRequest.message = err;
+                                                res.json(newRequest);
                                             } else {
-                                                res.json(request);
+                                                newRequest.state = 1;
+                                                res.json(newRequest);
                                             }
                                         })
                                     });
@@ -126,6 +130,10 @@ router.post('/login', function (req, res, next) {
     });
 //分页查询
 router.post('/getadmininfo', function (req, res, next) {
+   
+    
+    var newRequest = request;
+    console.log(newRequest);
     var arg = req.body;
     var phone = arg.phone;
     var pageNo = arg.page_no;
@@ -137,22 +145,24 @@ router.post('/getadmininfo', function (req, res, next) {
     data.connect(function (db) {
         db.collection('admin_info').find().toArray(function (err, docs) {
             if (err) {
-                request.state = -1;
-                request.message = err;
-                res.json(request);
+                newRequest.state = -1;
+                newRequest.message = err;
+                res.json(newRequest);
             } else {
-                request.total = docs.length;
+                newRequest.total = docs.length;
                 data.findByPage("admin_info", seachdata, pageNo, pageSize, function (err, docs2) {
                     if (err) {
-                        request.state = -1;
-                        request.message = err;
-                        res.json(request);
+                        newRequest.state = -1;
+                        newRequest.message = err;
+                        res.json(newRequest);
                     } else {
-                        console.log(docs2);
-                        request.data = docs2;
-                        request.pageSize = pageSize;
-                        request.pageNo = pageNo;
-                        res.json(request);
+                      
+                        newRequest.state = 1;
+                     
+                        newRequest.data = docs2;
+                        newRequest.pageSize = pageSize;
+                        newRequest.pageNo = pageNo;
+                        res.json(newRequest);
                     }
                 })
                 // data.connect(function(db){
@@ -164,6 +174,8 @@ router.post('/getadmininfo', function (req, res, next) {
 });
 //获取详情
 router.post('/getadmininfobyid', function (req, res, next) {
+   
+    var newRequest = request;
     var arg = req.body;
 
     var id = arg.id;
@@ -171,14 +183,14 @@ router.post('/getadmininfobyid', function (req, res, next) {
     data.connect(function (db) {
         db.collection('admin_info').find(seach).toArray(function (err, docs) {
             if (err) {
-                request.state = -1;
-                request.message = err;
-                res.json(request);
+                newRequest.state = -1;
+                newRequest.message = err;
+                res.json(newRequest);
                 
             }else{
-                request.state = 1;
-                request.data = docs;
-                res.json(request);
+                newRequest.state = 1;
+                newRequest.data = docs;
+                res.json(newRequest);
             }
         })
     })
@@ -186,42 +198,61 @@ router.post('/getadmininfobyid', function (req, res, next) {
 });
 //修改管理员信息
 router.post('/updateadmininfobyid', function (req, res, next) {
+   
+    var newRequest = request;
+    console.log(newRequest);
     var arg = req.body;
     var current_time = moment(Date.now()).format('YYYY-MM-DD HH:mm:ss');
     var update_id = { id: arg.id };
-    var update_data = {
+    var update_data = {$set:{
         name: arg.name,
         photo: arg.photo,
         remark: arg.remark,
         password: arg.password,
-        state: arg.state,
+        role_id:parseInt(arg.role_id),
         updatetime: current_time
-    }
-    data.connect(function (db) {
-        db.collection('admin_info').updateOne(update_id, update_data, function (err, result) {
-            if (err) {
-                request.state = -1;
-                request.message = err;
-                res.json(request);
-            } else {
-                res.json(request);
-            }
-        })
-    })
+    }}
+    
+    data.update('admin_info',update_id, update_data, function (err, result) {
+        if (err) {
+            newRequest.state = -1;
+            newRequest.message = err;
+            res.json(newRequest);
+        } else {
+            newRequest.state = 1;
+            newRequest.data = result;
+            console.log(result);
+            res.json(newRequest);
+        }
+    });
+    // data.connect(function (db) {
+    //     db.collection('admin_info').updateOne(update_id, update_data, function (err, result) {
+    //         if (err) {
+    //             request.state = -1;
+    //             request.message = err;
+    //             res.json(request);
+    //         } else {
+    //             request.state = 1;
+          
+    //             res.json(request);
+    //         }
+    //     })
+    // })
 });
 //批量删除
 router.post('/deleteadmininfobyids', function (req, res, next) {
+    var newRequest = request;
     var arg = req.body;
     var ids = arg.ids;
     data.connect(function (db) {
         db.collection('admin_info').deleteMany({ id: { $in: ids } }, function (err, result) {
             if (err) {
-                request.state = -1;
-                request.message = err;
-                res.json(request);
+                newRequest.state = -1;
+                newRequest.message = err;
+                res.json(newRequest);
             } else {
-                request.state = 1;
-                res.json(request);
+                newRequest.state = 1;
+                res.json(newRequest);
             }
         })
     });
